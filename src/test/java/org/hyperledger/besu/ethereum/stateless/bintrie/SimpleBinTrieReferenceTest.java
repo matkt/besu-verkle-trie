5a1cb8925853574d6d9fe3c66d7cff6053cd5692
Karim Taam <karim.t2am@gmail.com>
2025-08-07 17:26:07 +0400

    add ref tests
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

---
+++ src/test/java/org/hyperledger/besu/ethereum/stateless/bintrie/SimpleBinTrieReferenceTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright Hyperledger Besu Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+package org.hyperledger.besu.ethereum.stateless.bintrie;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.tuweni.bytes.Bytes32;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SimpleBinTrieReferenceTest {
+  static BytesPackedBitSequenceFactory keyFactory;
+
+  @BeforeAll
+  static void setup() {
+    keyFactory = new BytesPackedBitSequenceFactory();
+  }
+
+  @Test
+  public void testOneValue() {
+    SimpleBinTrie<BytesPackedBitSequence, Bytes32> trie = new SimpleBinTrie<>();
+    BytesPackedBitSequence key =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000000");
+    Bytes32 value =
+        Bytes32.fromHexString("0x0101010101010101010101010101010101010101010101010101010101010101");
+    trie.put(key, value);
+    Bytes32 expectedRootHash =
+        Bytes32.fromHexString("0x694545468677064fd833cddc8455762fe6b21c6cabe2fc172529e0f573181cd5");
+    assertThat(trie.getRootHash()).as("Retrieve root hash").isEqualByComparingTo(expectedRootHash);
+  }
+
+  @Test
+  public void testTwoEntriesDifferentFirstBit() {
+    SimpleBinTrie<BytesPackedBitSequence, Bytes32> trie = new SimpleBinTrie<>();
+    BytesPackedBitSequence key =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000000");
+    Bytes32 value =
+        Bytes32.fromHexString("0x0101010101010101010101010101010101010101010101010101010101010101");
+    trie.put(key, value);
+
+    BytesPackedBitSequence key2 =
+        keyFactory.fromHexString(
+            "0x8000000000000000000000000000000000000000000000000000000000000000");
+    Bytes32 value2 =
+        Bytes32.fromHexString("0x0202020202020202020202020202020202020202020202020202020202020202");
+    trie.put(key2, value2);
+    Bytes32 expectedRootHash =
+        Bytes32.fromHexString("0x85fc622076752a6fcda2c886c18058d639066a83473d9684704b5a29455ed2ed");
+    assertThat(trie.getRootHash()).as("Retrieve root hash").isEqualByComparingTo(expectedRootHash);
+  }
+
+  @Test
+  public void testOneStemColocatedValue() {
+    SimpleBinTrie<BytesPackedBitSequence, Bytes32> trie = new SimpleBinTrie<>();
+    BytesPackedBitSequence key =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000003");
+    Bytes32 value =
+        Bytes32.fromHexString("0x0101010101010101010101010101010101010101010101010101010101010101");
+    trie.put(key, value);
+
+    BytesPackedBitSequence key2 =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000004");
+    Bytes32 value2 =
+        Bytes32.fromHexString("0x0202020202020202020202020202020202020202020202020202020202020202");
+    trie.put(key2, value2);
+
+    BytesPackedBitSequence key3 =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000009");
+    Bytes32 value3 =
+        Bytes32.fromHexString("0x0303030303030303030303030303030303030303030303030303030303030303");
+    trie.put(key3, value3);
+
+    BytesPackedBitSequence key4 =
+        keyFactory.fromHexString(
+            "0x00000000000000000000000000000000000000000000000000000000000000FF");
+    Bytes32 value4 =
+        Bytes32.fromHexString("0x0404040404040404040404040404040404040404040404040404040404040404");
+    trie.put(key4, value4);
+    Bytes32 expectedRootHash =
+        Bytes32.fromHexString("0xaa12acb5689a2dc03e9d7ab0350449c70cdad286750dc8ba1dd092f5e100191a");
+    assertThat(trie.getRootHash()).as("Retrieve root hash").isEqualByComparingTo(expectedRootHash);
+  }
+
+  @Test
+  public void testTwoStemColocatedValue() {
+    SimpleBinTrie<BytesPackedBitSequence, Bytes32> trie = new SimpleBinTrie<>();
+    BytesPackedBitSequence key =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000003");
+    Bytes32 value =
+        Bytes32.fromHexString("0x0101010101010101010101010101010101010101010101010101010101010101");
+    trie.put(key, value);
+
+    BytesPackedBitSequence key2 =
+        keyFactory.fromHexString(
+            "0x0000000000000000000000000000000000000000000000000000000000000004");
+    Bytes32 value2 =
+        Bytes32.fromHexString("0x0202020202020202020202020202020202020202020202020202020202020202");
+    trie.put(key2, value2);
+
+    BytesPackedBitSequence key3 =
+        keyFactory.fromHexString(
+            "0x8000000000000000000000000000000000000000000000000000000000000003");
+    Bytes32 value3 =
+        Bytes32.fromHexString("0x0101010101010101010101010101010101010101010101010101010101010101");
+    trie.put(key3, value3);
+
+    BytesPackedBitSequence key4 =
+        keyFactory.fromHexString(
+            "0x8000000000000000000000000000000000000000000000000000000000000004");
+    Bytes32 value4 =
+        Bytes32.fromHexString("0x0202020202020202020202020202020202020202020202020202020202020202");
+    trie.put(key4, value4);
+    Bytes32 expectedRootHash =
+        Bytes32.fromHexString("0x6387c711dfeaa802e67f83bedb504d42e1bc89f64c4e44aa69d207351b2a03e5");
+    assertThat(trie.getRootHash()).as("Retrieve root hash").isEqualByComparingTo(expectedRootHash);
+  }
+}
